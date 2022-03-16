b23a6fb6f3b67afba44cd0f4001879d84e377ccd
sunnatoff <kamronbeyraxmatov@gmail.com>
2022-03-16 12:19:03 +0500

    catalog add

---
+++ src/Pages/Katalog/Katalog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Katalog() {
+    return (
+        
+        <section className='c-katolog'>
+        <h1>Katalog</h1>
+        <h2>slom</h2>
+        </section>
+    )
+}
+
+export default Katalog
